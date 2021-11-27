d481dabee391a97fa2c3c74c8ea6eb805a153caa
Adam Hall <adamhalldesigns@gmail.com>
2021-11-27 18:00:22 +0000

    basic app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
-import type { Component } from "solid-js";
-
-import logo from "./logo.svg";
+import { Component, createResource, Index, Match, Switch } from "solid-js";
+import Parser, { Tree } from "web-tree-sitter";
 import styles from "./App.module.css";
 
 const App: Component = () => {
+  const [parser] = createResource(async () => {
+    await Parser.init();
+    const lang = await Parser.Language.load("tree-sitter-javascript.wasm");
+    const parser = new Parser();
+    parser.setLanguage(lang);
+    return parser;
+  });
+
   return (
     <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+      <Switch fallback={<Page parser={parser() as Parser} />}>
+        <Match when={parser.error}>
+          <pre>{parser.error}</pre>
+        </Match>
+        <Match when={parser.loading}>
+          <p>loading...</p>
+        </Match>
+      </Switch>
+    </div>
+  );
+};
+
+const Page: Component<{ parser: Parser }> = ({ parser }) => {
+  const code = "let x = 1; console.log(x);";
+  const tree = parser.parse(code);
+  return (
+    <div class={styles.page}>
+      <div class={styles.header}>Tree-sitter Playground</div>
+      <div class={`${styles.query} ${styles.borderBox}`}>query</div>
+      <div class={`${styles.source} ${styles.borderBox}`}>{code}</div>
+      <div class={`${styles.tree} ${styles.borderBox}`}>
+        {tree.rootNode.toString()}
+      </div>
+      <div class={`${styles.output} ${styles.borderBox}`}>output</div>
     </div>
   );
 };
